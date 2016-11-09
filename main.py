f47259960827d3c6c5e5ed95fc1176804aee6108
Nico <nsuarez@umich.edu>
2016-11-09 03:15:45 -0500

    Hey I should put this site on GitHub

---
+++ main.py
@@ -0,0 +1,20 @@
+import webapp2
+import os
+import logging
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+        infoDict = {'result': 'Please, no', 'yes': False}
+        self.response.write(template.render(infoDict))
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/index.html', MainHandler)
+], debug=True)
